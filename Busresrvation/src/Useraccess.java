327b6c6810e76fc27e5daaae476c27c7d9df2a26
Munesh29 <117075788+Munesh29@users.noreply.github.com>
2022-10-31 15:27:56 +0530

    Update Useraccess.java

--- Busresrvation/src/Useraccess.java
+++ Busresrvation/src/Useraccess.java
@@ -5,12 +5,8 @@ public class Useraccess {
         boolean run=true;
         while(run)
         {
-            
-        //Scanner scan=new Scanner(System.in); 
-            
         UserData u=new UserData();
         System.out.println("\nIf you are new user type \"yes\" or \"no\"");
-        //String st=getuserstring();
             String st=scan.next();
         if(st.equalsIgnoreCase("Yes"))
         {
@@ -27,16 +23,10 @@ public class Useraccess {
             
         System.out.println("\nIf you want to booking ticket enter \"1\"\nIf you want to cancel Ticket enter \"2\"");
         System.out.println("If you want to see your previous Ticket Booking list enter \"3\"\nIf you want to logout enter \"0\"");
-      
-        //int n=getuserchoice();
         String p=scan.next().trim();
-        //System.out.println("shidsjf");
             if(p.equals("1")){
             TicketBooking tk=new TicketBooking();
             again=tk.ticketbooking();
-            //System.out.println("run "+again);
-
-            
         }
         else if(p.equals("2")){
             CancelTicket cl=new CancelTicket();
@@ -48,11 +38,8 @@ public class Useraccess {
         else if(p.equals("0")){
             again=false;
         }
-    }
-    
+        }
         }  
-       // scan.close();
     }
-    
 }
 
